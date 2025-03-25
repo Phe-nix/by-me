33c4df235301f52b8aae5400c6bdf2dab4d54210
Phe-nix <spicedexdee123@gmail.com>
2025-03-26 01:56:38 +0700

    ⚡️ test

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,22 +3,12 @@
   import { goto } from "$app/navigation";
   import { PUBLIC_BASE_API } from "$env/static/public";
   import axios from "axios";
-
-  let email = $state()
-  let password = $state()
-  const login = async() => {
+    
+    const login = async() => {
         try {
             const req = await axios.post(`${PUBLIC_BASE_API}/auth/signin`, {
-                email,
-                password
-            })
 
-            if (req.status == 200) {
-                goto('/home')
-            }
-            else {
-                alert('something was wrong')
-            }
+            })
         }
         catch (e) {
             return e.message
@@ -27,19 +17,25 @@
 
 </script>
 <div class="flex flex-col justify-center items-center gap-5">
-    <h1>Login</h1>
+    <h1>Register</h1>
+    <div class="flex flex-col items-center">
+        <p>FirstName</p>
+        <input class="" type="text">
+    </div>
+    <div class="flex flex-col items-center">
+        <p>LastName</p>
+        <input class="" type="text">
+    </div>
     <div class="flex flex-col items-center">
         <p>Email</p>
-        <input class="" type="email" bind:value={email}>
+        <input class="" type="text">
     </div>
     <div class="flex flex-col items-center">
         <p>Password</p>
-        <input class="" type="password" bind:value={password}>
+        <input class="" type="text">
     </div>
+    <button class="border p-2 rounded-md">Register</button>
     <button class="border p-2 rounded-md" onclick={() => {
-        login()
-    }}>Login</button>
-    <button class="border p-2 rounded-md" onclick={() => {
-        goto('/register')
-    }}>Register👉</button>
+        goto('/')
+    }}>Login👉</button>
 </div>
